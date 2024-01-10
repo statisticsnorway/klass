afe426876bf00beb5525cec3d4e4838a6f28e4d4
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2024-01-10 13:18:34 +0100

    Add test

---
+++ klass-api/src/test/java/no/ssb/klass/api/dto/hal/ResourceUtilTest.java
@@ -0,0 +1,25 @@
+package no.ssb.klass.api.dto.hal;
+
+import no.ssb.klass.api.controllers.ClassificationController;
+import org.junit.jupiter.api.Test;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ResourceUtilTest {
+    @Test
+    public void createSearchLinkUsingTemplate() {
+        WebMvcLinkBuilder linkBuilder = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(ClassificationController.class)
+                .search("query", null, true, null, null));
+        String link = ResourceUtil.createUriTemplate(linkBuilder, "query", "includeCodelists").toString();
+        assertEquals("/v1/classifications/search{?query,includeCodelists}", link);
+    }
+    @Test
+    public void createSearchLinkWithPathSubstitution() {
+        WebMvcLinkBuilder linkBuilder = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(ClassificationController.class)
+                .search("query", null, true, null, null));
+        String link = ResourceUtil.createUriTemplateBuilder(linkBuilder).basePath("/api/klass/")
+                .variables("query", "includeCodelists").build().toString();
+        assertEquals("/api/klass/v1/classifications/search{?query,includeCodelists}", link);
+    }
+}
