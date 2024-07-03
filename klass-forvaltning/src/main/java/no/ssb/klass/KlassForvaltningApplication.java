4ad9dc05cf9cc18f6adebeb6f061cb43d61a3168
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2024-07-03 14:48:23 +0200

    Explicitly deregister JDBC driver (#181)
    * Explicitly deregister JDBC driver
    
    * Remove first solution attempt

--- klass-forvaltning/src/main/java/no/ssb/klass/KlassForvaltningApplication.java
+++ klass-forvaltning/src/main/java/no/ssb/klass/KlassForvaltningApplication.java
@@ -7,14 +7,39 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
-// CHECKSTYLE:OFF
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletException;
+
+
 @SpringBootApplication
 @Import(EmbeddedServletContainerAutoConfiguration.EmbeddedTomcat.class)
 public class KlassForvaltningApplication extends SpringBootServletInitializer {
-    // TODO kmgv if using embedded container (e.g. Tomcat) remove below method and extends SpringBootServletInitializer
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(KlassForvaltningApplication.class);
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        servletContext.addListener(new ServletContextListener() {
+
+            @Override
+            public void contextInitialized(ServletContextEvent servletContextEvent) {
+                // Nothing to do here
+            }
+
+            @Override
+            public void contextDestroyed(ServletContextEvent sce) {
+                // Explicitly deregister the driver to prevent race conditions with Tomcat de-registering the Driver.
+                // This is fixed in Spring Boot versions >=2.3.0
+                // Ref https://github.com/spring-projects/spring-boot/issues/21221
+                org.mariadb.jdbc.Driver.unloadDriver();
+            }
+        });
+        super.onStartup(servletContext);
     }
 
 
@@ -22,4 +47,3 @@ public class KlassForvaltningApplication extends SpringBootServletInitializer {
         SpringApplication.run(KlassForvaltningApplication.class, args);
     }
 }
-// CHECKSTYLE:ON
