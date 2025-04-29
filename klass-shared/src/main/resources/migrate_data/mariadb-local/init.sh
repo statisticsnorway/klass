773886d8fd1c44cc2d5bdfd53977083bcb697b99
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-04-29 16:16:04 +0200

    move files

--- klass-shared/src/main/resources/migrate_data/mariadb-local/init.sh
+++ klass-shared/src/main/resources/migrate_data/mariadb-local/init.sh
@@ -1,3 +1,8 @@
+# Load variables from .env
+set -a
+source .env
+set +a
+
 docker compose up -d
 sleep 5
 docker compose exec -T mariadb mariadb -u root -p"$MARIADB_ROOT_PASSWORD" --host=127.0.0.1 klass < "$MARIADB_LOCAL_FILEPATH"
--- klass-shared/src/main/resources/migrate_data/postgres-local/dump_postgres.sh
+++ klass-shared/src/main/resources/migrate_data/postgres-local/dump_postgres.sh
@@ -1,6 +1,9 @@
+# Load env values from file
 set -a
 source .env
 set +a
 
+# Set pg_dump env password
 export PGPASSWORD=${POSTGRES_PASSWORD}
+# Dump data to file
 pg_dump -U klass -h localhost -p 5432 klass > "$POSTGRES_LOCAL_FILEPATH"/klass-postgres-dump.sql
