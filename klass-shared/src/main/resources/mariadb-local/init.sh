6fe5d11a1f3e6bb2193c8c7b30c4e26fad4bb936
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-04-29 15:11:12 +0200

    add env value

--- klass-shared/src/main/resources/mariadb-local/init.sh
+++ klass-shared/src/main/resources/mariadb-local/init.sh
@@ -1,3 +1,3 @@
 docker compose up -d
 sleep 5
-docker compose exec -T mariadb mariadb -u root -prootpassword --host=127.0.0.1 klass < "$MARIADB_LOCAL_FILEPATH"
+docker compose exec -T mariadb mariadb -u root -p"$MARIADB_ROOT_PASSWORD" --host=127.0.0.1 klass < "$MARIADB_LOCAL_FILEPATH"
