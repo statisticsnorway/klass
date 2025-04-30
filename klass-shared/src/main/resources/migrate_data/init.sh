924df64731b29b2358c01187c2f93e3f96266935
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-04-30 08:57:37 +0200

    refactor structure - run all jobs sequentially

--- klass-shared/src/main/resources/migrate_data/init.sh
+++ klass-shared/src/main/resources/migrate_data/init.sh
@@ -11,3 +11,9 @@ sleep 5
 
 # Populate mariadb database with data from file
 docker compose exec -T mariadb mariadb -u root -p"$MARIADB_ROOT_PASSWORD" --host=127.0.0.1 klass < "$MARIADB_LOCAL_FILEPATH"
+
+# Migrate data from mariadb database to postgres database
+./pgloader/load.sh
+
+# Dump data from postgres database to file
+pg_dump -U klass -h localhost -p 5432 klass > "$POSTGRES_LOCAL_FILEPATH"/"$POSTGRES_DUMP_FILENAME"
