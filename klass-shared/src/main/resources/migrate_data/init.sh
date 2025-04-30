2e0049fb0b217b541533719b248468bb8bb0582e
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-04-30 08:33:51 +0200

    combine containers

--- klass-shared/src/main/resources/migrate_data/init.sh
+++ klass-shared/src/main/resources/migrate_data/init.sh
@@ -3,6 +3,11 @@ set -a
 source .env
 set +a
 
+# Start containers
 docker compose up -d
+
+# Wait
 sleep 5
+
+# Populate mariadb database with data from file
 docker compose exec -T mariadb mariadb -u root -p"$MARIADB_ROOT_PASSWORD" --host=127.0.0.1 klass < "$MARIADB_LOCAL_FILEPATH"
