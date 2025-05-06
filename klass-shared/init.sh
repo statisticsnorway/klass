fe6bf9090e1882f183671728695d61f1baa3a143
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-05-06 15:22:44 +0200

    docker compose

--- klass-shared/init.sh
+++ klass-shared/init.sh
@@ -1,28 +1,47 @@
+#!/bin/bash
+
 # Load variables from .env
 set -a
 source .env
 set +a
 
 # Start containers
-docker compose up -d --build
+docker compose up -d
+
+#--build
 
 # Wait
-#sleep 5
+sleep 10
 
-echo "Waiting for MariaDB to be ready..."
-until docker compose exec mariadb mariadb-admin -uroot -p"rootpassword" ping --silent; do
-  sleep 1
-done
+#echo "Waiting for MariaDB to be ready..."
+#until docker compose exec mariadb mariadb-admin -uroot -p"rootpassword" ping --silent; do
+#  sleep 1
+#done
 
 # Populate mariadb database with data from file
-docker compose exec -T mariadb mariadb -u root -p"rootpassword" --host=127.0.0.1 klass < "$MARIADB_LOCAL_FILEPATH"
+docker compose exec -T mariadb mariadb -u root -p"$MARIADB_ROOT_PASSWORD" --host=127.0.0.1 klass < "$MARIADB_LOCAL_FILEPATH"
 
+sleep 10
 # Migrate data from mariadb database to postgres database
 migrate_data/pgloader/load.sh
 
+# Check the data is indeed migrated/tables are created
+sleep 10
+
+docker cp alter_timestamp.sql "$(docker compose ps -q postgresql)":/tmp/alter_timestamp.sql
+docker compose exec -T postgresql psql -U klass -d klass -f /tmp/alter_timestamp.sql
+
+#docker compose exec -T postgresql psql -U klass -d klass <<EOF
+#SELECT table_schema,
+#       table_name,
+#       privilege_type
+#  FROM information_schema.role_table_grants
+#  WHERE grantee = 'klass' AND table_schema = 'klass';
+#EOF
 #echo "Dump postgres"
 # Dump data from postgres database to file
 #pg_dump -U klass -h localhost -p 5432 klass > "$POSTGRES_LOCAL_FILEPATH"/"$POSTGRES_DUMP_FILENAME"
 
+docker compose build
 #echo "Restart klass-api"
-#docker compose restart klass-api
+docker compose restart klass-api
