c869b1582efcab9846c703cfb6a45fd18e69ec82
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-05-07 13:29:08 +0200

    Separate init scripts

--- klass-shared/init-migration.sh
+++ klass-shared/init-migration.sh
@@ -8,32 +8,25 @@ set +a
 # Start containers
 docker compose up -d
 
-# Wait for mariadb to be ready
+echo "Wait for mariadb to be ready"
 sleep 10
 
-# Populate mariadb database with data from file
+echo "Load data from file to mariadb database"
 docker compose exec -T mariadb mariadb -u root -p"$MARIADB_ROOT_PASSWORD" --host=127.0.0.1 klass < "$MARIADB_LOCAL_FILEPATH"
 
-# Wait for load from file to finish
+echo "Wait for load from file to finish"
 sleep 10
 
-# Migrate data from mariadb database to postgres database
+echo "Migrate data from mariadb database to postgres database"
 migrate_data/pgloader/load.sh
 
-# Wait for migration to finish
+echo "Wait for migration to finish"
 sleep 10
 
 # Run necessary postgresql alteration on type timestamp to remove microseconds
+echo "Alter columns of type timestamp"
 docker cp alter_timestamp.sql "$(docker compose ps -q postgresql)":/tmp/alter_timestamp.sql
 docker compose exec -T postgresql psql -U klass -d klass -f /tmp/alter_timestamp.sql
 
-#echo "Dump postgres"
-# Dump data from postgres database to file
+echo "Dump data from postgres database to file"
 pg_dump -U klass -h localhost -p 5432 klass > "$POSTGRES_LOCAL_FILEPATH"/"$POSTGRES_DUMP_FILENAME"
-
-# Build klass-api image
-docker compose build
-
-# Restart klass-api because the application will crash when db are not ready
-#echo "Restart klass-api"
-docker compose restart klass-api
