63de6923f703a5ef79ed25abd5a8d393568c7a3e
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-05-15 16:12:11 +0200

    refactor test class

--- klass-api/src/test/java/no/ssb/klass/api/dataintegrity/KlassApiClassificationsTest.java
+++ klass-api/src/test/java/no/ssb/klass/api/dataintegrity/KlassApiClassificationsTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class KlassApiClassificationsTest {
     final String basePath = "/api/klass";
@@ -18,73 +19,60 @@ public class KlassApiClassificationsTest {
     private final String klassApiPostgresDBHost = "http://localhost:8080";
     public String klassApiMariaDBPath = klassApiMariaDBHost + basePath + RestConstants.API_VERSION_V1 + "/classifications";
     public String klassApiPostgresDBPath = klassApiPostgresDBHost + basePath + RestConstants.API_VERSION_V1 +  "/classifications";
-    List<Map<String, Object>> mariaDBClassificationsPage0;
-    List<Map<String, Object>> postgresDBClassificationsPage0;
+    List<Map<String, Object>> mariaDBClassificationsPage;
+    List<Map<String, Object>> postgresDBClassificationsPage;
 
     @BeforeEach
     void setUp() {
         responseKlassApiMariaDB = RestAssured.get(klassApiMariaDBPath);
         responseKlassApiPostgresDB = RestAssured.get(klassApiPostgresDBPath);
-        mariaDBClassificationsPage0 = responseKlassApiMariaDB.path("_embedded.classifications");
-        postgresDBClassificationsPage0 = responseKlassApiPostgresDB.path("_embedded.classifications");
+        mariaDBClassificationsPage = responseKlassApiMariaDB.path("_embedded.classifications");
+        postgresDBClassificationsPage = responseKlassApiPostgresDB.path("_embedded.classifications");
     }
 
     @Test
     void getClassificationsResultIsEqualSize() {
-        assertThat(responseKlassApiMariaDB.getStatusCode()).isEqualTo(200);
-        assertThat(responseKlassApiPostgresDB.getStatusCode()).isEqualTo(200);
+        assumeTrue(responseKlassApiMariaDB.getStatusCode() == 200, "MariaDB API not available");
+        assumeTrue(responseKlassApiPostgresDB.getStatusCode() == 200, "Postgres API not available");
+
         int klassApiMariaDBTotalElements = responseKlassApiMariaDB.path("page.totalElements");
         int klassApiPostgresDBTotalElements = responseKlassApiPostgresDB.path("page.totalElements");
+
         assertThat(klassApiMariaDBTotalElements).isEqualTo(klassApiPostgresDBTotalElements);
     }
 
     @Test
     void getClassificationsPageSizeIsEqual(){
-        assertThat(mariaDBClassificationsPage0.size()).isEqualTo(postgresDBClassificationsPage0.size());
+        assumeTrue(responseKlassApiMariaDB.getStatusCode() == 200, "MariaDB API not available");
+        assumeTrue(responseKlassApiPostgresDB.getStatusCode() == 200, "Postgres API not available");
+
+        assertThat(mariaDBClassificationsPage.size()).isEqualTo(postgresDBClassificationsPage.size());
+        String mariaDBClassificationsPage = responseKlassApiMariaDB.path("_embedded.page");
+        String postgresDBClassificationsPage = responseKlassApiPostgresDB.path("_embedded.page");
+        assertThat(mariaDBClassificationsPage).isEqualTo(postgresDBClassificationsPage);
     }
 
     @Test
-    void getClassificationsNamesAreEqual(){
-        for (int i = 0; i < mariaDBClassificationsPage0.size(); i++) {
-            assertThat(mariaDBClassificationsPage0.get(i).get("name")).isEqualTo(postgresDBClassificationsPage0.get(i).get("name"));
+    void getClassificationsItems(){
+        assumeTrue(responseKlassApiMariaDB.getStatusCode() == 200, "MariaDB API not available");
+        assumeTrue(responseKlassApiPostgresDB.getStatusCode() == 200, "Postgres API not available");
+
+        for (int i = 0; i < mariaDBClassificationsPage.size(); i++) {
+            assertThat(
+                    mariaDBClassificationsPage.get(i).get("name")).isEqualTo(
+                            postgresDBClassificationsPage.get(i).get("name"));
+            assertThat(
+                    mariaDBClassificationsPage.get(i).get("id")).isEqualTo(
+                            postgresDBClassificationsPage.get(i).get("id"));
+            assertThat(
+                    mariaDBClassificationsPage.get(i).get("classificationType")).isEqualTo(
+                            postgresDBClassificationsPage.get(i).get("classificationType"));
+
+            Map<String, Object> classification = mariaDBClassificationsPage.get(i);
+            assertThat(classification.get("_links")).isNotEqualTo(
+                    postgresDBClassificationsPage.get(i).get("_links"));
         }
 
-    }
-
-    @Test
-    void getClassificationsIdsAreEqual(){
-        for (int i = 0; i < mariaDBClassificationsPage0.size(); i++) {
-            assertThat(mariaDBClassificationsPage0.get(i).get("id")).isEqualTo(postgresDBClassificationsPage0.get(i).get("id"));
-        }
-
-    }
-
-    @Test
-    void getClassificationsClassificationTypesAreEqual(){
-        for (int i = 0; i < mariaDBClassificationsPage0.size(); i++) {
-            assertThat(mariaDBClassificationsPage0.get(i).get("classificationType")).isEqualTo(postgresDBClassificationsPage0.get(i).get("classificationType"));
-        }
-
-    }
-
-    @Test
-    void getClassificationsLastModifiedAreEqual(){
-        for (int i = 0; i < mariaDBClassificationsPage0.size(); i++) {
-            assertThat(mariaDBClassificationsPage0.get(i).get("lastModified")).isEqualTo(postgresDBClassificationsPage0.get(i).get("lastModified"));
-        }
-
-    }
-
-    @Test
-    void getClassificationsLinksAreNotEqual(){
-        for (int i = 0; i < mariaDBClassificationsPage0.size(); i++) {
-            Map<String, Object> classification = mariaDBClassificationsPage0.get(i);
-            assertThat(classification.get("_links")).isNotEqualTo(postgresDBClassificationsPage0.get(i).get("_links"));
-        }
-    }
-
-    @Test
-    void getClassificationsChangedSinceAreEqual(){
     }
 
 }
