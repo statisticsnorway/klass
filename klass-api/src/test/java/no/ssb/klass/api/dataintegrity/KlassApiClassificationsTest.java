cc92737df9ae39b3c9274d500b445a14595e6b26
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-05-15 13:20:41 +0200

    add tests

--- klass-api/src/test/java/no/ssb/klass/api/dataintegrity/KlassApiClassificationsTest.java
+++ klass-api/src/test/java/no/ssb/klass/api/dataintegrity/KlassApiClassificationsTest.java
@@ -5,6 +5,8 @@ import io.restassured.response.Response;
 import no.ssb.klass.api.util.RestConstants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -17,10 +19,17 @@ public class KlassApiClassificationsTest {
     public String klassApiMariaDBPath = klassApiMariaDBHost + basePath + RestConstants.API_VERSION_V1 + "/classifications";
     public String klassApiPostgresDBPath = klassApiPostgresDBHost + basePath + RestConstants.API_VERSION_V1 +  "/classifications";
 
+    List<Map<String, Object>> mariaDBClassificationsPages;
+    List<Map<String, Object>> mariaDBClassificationsPage0;
+    List<Map<String, Object>> postgresDBClassificationsPage0;
+    List<Map<String, Object>> postgresDBClassificationsPages;
+
     @BeforeEach
     void setUp() {
         responseKlassApiMariaDB = RestAssured.get(klassApiMariaDBPath);
         responseKlassApiPostgresDB = RestAssured.get(klassApiPostgresDBPath);
+        mariaDBClassificationsPage0 = responseKlassApiMariaDB.path("_embedded.classifications");
+        postgresDBClassificationsPage0 = responseKlassApiPostgresDB.path("_embedded.classifications");
     }
 
     @Test
@@ -30,6 +39,19 @@ public class KlassApiClassificationsTest {
         int klassApiMariaDBTotalElements = responseKlassApiMariaDB.path("page.totalElements");
         int klassApiPostgresDBTotalElements = responseKlassApiPostgresDB.path("page.totalElements");
         assertThat(klassApiMariaDBTotalElements).isEqualTo(klassApiPostgresDBTotalElements);
+    }
+
+    @Test
+    void getClassificationsPageSizeIsEqual(){
+        assertThat(mariaDBClassificationsPage0.size()).isEqualTo(postgresDBClassificationsPage0.size());
+    }
+
+    @Test
+    void getClassificationsResult(){
+        for (int i = 0; i < mariaDBClassificationsPage0.size(); i++) {
+            assertThat(mariaDBClassificationsPage0.get(i).get("name")).isEqualTo(postgresDBClassificationsPage0.get(i).get("name"));
+        }
+
     }
 
 }
