2857bb4de7465c9aaa65e47d42be1eef8ab773f1
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-05-23 16:10:13 +0200

    add helper method

--- klass-api/src/test/java/no/ssb/klass/api/migration/MigrationTestUtils.java
+++ klass-api/src/test/java/no/ssb/klass/api/migration/MigrationTestUtils.java
@@ -1,4 +1,14 @@
 package no.ssb.klass.api.migration;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class MigrationTestUtils {
+
+    public static Map<Object, Map<String, Object>> mapById(List<Map<String, Object>> versions) {
+        return versions.stream()
+                .collect(Collectors.toMap(v -> v.get("id"), Function.identity()));
+    }
 }
