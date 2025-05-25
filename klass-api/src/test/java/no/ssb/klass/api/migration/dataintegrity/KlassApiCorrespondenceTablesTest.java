098a6c41c0f89edd952e3adc9f9583c6af74473c
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-05-25 16:00:24 +0200

    add tests

---
+++ klass-api/src/test/java/no/ssb/klass/api/migration/dataintegrity/KlassApiCorrespondenceTablesTest.java
@@ -0,0 +1,40 @@
+package no.ssb.klass.api.migration.dataintegrity;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static no.ssb.klass.api.migration.MigrationTestConstants.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class KlassApiCorrespondenceTablesTest extends AbstractKlassApiDataIntegrityTest {
+
+
+    @ParameterizedTest
+    @MethodSource("rangeProvider")
+    void getCorrespondenceTable(Integer correspondenceTableId) {
+        Response sourceResponse = klassApiMigrationClient.getFromSourceApi("/" + CORRESPONDENCE_TABLES + "/" + correspondenceTableId, null);
+        Response targetResponse = klassApiMigrationClient.getFromTargetApi("/" + CORRESPONDENCE_TABLES + "/" + correspondenceTableId, null);
+
+        assertThat(targetResponse).isNotNull();
+        assertThat(sourceResponse).isNotNull();
+
+        if(sourceResponse.getStatusCode() != 200) {
+            System.out.println(LOG_MESSAGE_STATUS_CODE + sourceResponse.getStatusCode());
+            assertThat(compareError(correspondenceTableId, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            System.out.println(LOG_MESSAGE_STATUS_CODE + sourceResponse.getStatusCode());
+
+            validateItem(sourceResponse, targetResponse, pathNamesCorrespondenceTableById);
+            validateLinks(sourceResponse, targetResponse, pathNamesCorrespondencesLinks);
+        }
+    }
+
+    static Stream<Integer> rangeProvider() {
+        return IntStream.rangeClosed(0, 1500).boxed();
+    }
+}
