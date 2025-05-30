68df896817fca5509e75d9d484c599fa9ced340e
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-05-30 14:09:59 +0200

    test all get classification families by id

--- klass-api/src/test/java/no/ssb/klass/api/migration/dataintegrity/KlassApiClassificationFamiliesByIdTest.java
+++ klass-api/src/test/java/no/ssb/klass/api/migration/dataintegrity/KlassApiClassificationFamiliesByIdTest.java
@@ -1,11 +1,14 @@
 package no.ssb.klass.api.migration.dataintegrity;
 
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -15,8 +18,23 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class KlassApiClassificationFamiliesByIdTest extends KlassApiClassificationCorrespondsAtTest {
 
+    static Map<String, Object> paramsIncludeCodeLists= new HashMap<>();
+    static Map<String, Object> paramsLanguageEn = new HashMap<>();
+    static Map<String, Object> paramsLanguageNn = new HashMap<>();
+    static Map<String, Object> paramsSsbSection = new HashMap<>();
+
+    static int randomSsbSectionId;
+
+    @BeforeAll
+    static void beforeAllClassificationFamiliesById() {
+        paramsIncludeCodeLists.put(INCLUDE_CODE_LISTS, TRUE);
+        paramsLanguageEn.put(LANGUAGE, EN);
+        paramsLanguageNn.put(LANGUAGE, NN);
+
+        randomSsbSectionId = generateRandomId(ssbSectionNames.size());
+    }
     @Test
-    void getOneClassificationFamilyBy() {
+    void getOneClassificationFamilyById() {
         int classificationFamilyId = 11;
         String path = getClassificationFamilyByIdPath(classificationFamilyId);
         Response sourceResponse = klassApiMigrationClient.getFromSourceApi(path, null,null);
@@ -37,7 +55,7 @@ public class KlassApiClassificationFamiliesByIdTest extends KlassApiClassificati
 
     @ParameterizedTest
     @MethodSource("rangeProviderClassificationFamilyIds")
-    void getClassificationFamilyBy(int classificationFamilyId) {
+    void getClassificationFamilyById(int classificationFamilyId) {
         String path = getClassificationFamilyByIdPath(classificationFamilyId);
         Response sourceResponse = klassApiMigrationClient.getFromSourceApi(path, null,null);
         Response targetResponse = klassApiMigrationClient.getFromTargetApi(path , null,null);
@@ -57,10 +75,174 @@ public class KlassApiClassificationFamiliesByIdTest extends KlassApiClassificati
 
     @ParameterizedTest
     @MethodSource("rangeProviderClassificationFamilyIds")
-    void getClassificationFamilyByXml(int classificationFamilyId) {
+    void getClassificationFamilyByIdXml(int classificationFamilyId) {
         String path = getClassificationFamilyByIdPath(classificationFamilyId);
         Response sourceResponse = klassApiMigrationClient.getFromSourceApi(path, null,TEXT_XML);
         Response targetResponse = klassApiMigrationClient.getFromTargetApi(path , null,TEXT_XML);
+
+        assertApiResponseIsNotNull(sourceResponse);
+
+        assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+
+        if(sourceResponse.getStatusCode() != 200) {
+            assertThat(compareError(null, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            validateItems(sourceResponse, targetResponse, pathNamesClassificationFamilyById);
+            validatePathListWithObjects(sourceResponse, targetResponse, CLASSIFICATIONS, pathNamesClassificationsPage, ID);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("rangeProviderClassificationFamilyIds")
+    void getClassificationFamilyIncludeCodeLists(int classificationFamilyId) {
+        String path = getClassificationFamilyByIdPath(classificationFamilyId);
+        Response sourceResponse = klassApiMigrationClient.getFromSourceApi(path, paramsIncludeCodeLists,null);
+        Response targetResponse = klassApiMigrationClient.getFromTargetApi(path , paramsIncludeCodeLists,null);
+
+        assertApiResponseIsNotNull(sourceResponse);
+
+        assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+
+        if(sourceResponse.getStatusCode() != 200) {
+            assertThat(compareError(null, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            validateItems(sourceResponse, targetResponse, pathNamesClassificationFamilyById);
+            validatePathListWithObjects(sourceResponse, targetResponse, CLASSIFICATIONS, pathNamesClassificationsPage, ID);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("rangeProviderClassificationFamilyIds")
+    void getClassificationFamilyIncludeCodeListsXml(int classificationFamilyId) {
+        String path = getClassificationFamilyByIdPath(classificationFamilyId);
+        Response sourceResponse = klassApiMigrationClient.getFromSourceApi(path, paramsIncludeCodeLists,TEXT_XML);
+        Response targetResponse = klassApiMigrationClient.getFromTargetApi(path , paramsIncludeCodeLists,TEXT_XML);
+
+        assertApiResponseIsNotNull(sourceResponse);
+
+        assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+
+        if(sourceResponse.getStatusCode() != 200) {
+            assertThat(compareError(null, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            validateItems(sourceResponse, targetResponse, pathNamesClassificationFamilyById);
+            validatePathListWithObjects(sourceResponse, targetResponse, CLASSIFICATIONS, pathNamesClassificationsPage, ID);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("rangeProviderClassificationFamilyIds")
+    void getClassificationFamilyEnglish(int classificationFamilyId) {
+        String path = getClassificationFamilyByIdPath(classificationFamilyId);
+        Response sourceResponse = klassApiMigrationClient.getFromSourceApi(path, paramsLanguageEn,null);
+        Response targetResponse = klassApiMigrationClient.getFromTargetApi(path , paramsLanguageEn,null);
+
+        assertApiResponseIsNotNull(sourceResponse);
+
+        assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+
+        if(sourceResponse.getStatusCode() != 200) {
+            assertThat(compareError(null, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            validateItems(sourceResponse, targetResponse, pathNamesClassificationFamilyById);
+            validatePathListWithObjects(sourceResponse, targetResponse, CLASSIFICATIONS, pathNamesClassificationsPage, ID);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("rangeProviderClassificationFamilyIds")
+    void getClassificationFamilyEnglishXml(int classificationFamilyId) {
+        String path = getClassificationFamilyByIdPath(classificationFamilyId);
+        Response sourceResponse = klassApiMigrationClient.getFromSourceApi(path, paramsLanguageEn,TEXT_XML);
+        Response targetResponse = klassApiMigrationClient.getFromTargetApi(path , paramsLanguageEn,TEXT_XML);
+
+        assertApiResponseIsNotNull(sourceResponse);
+
+        assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+
+        if(sourceResponse.getStatusCode() != 200) {
+            assertThat(compareError(null, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            validateItems(sourceResponse, targetResponse, pathNamesClassificationFamilyById);
+            validatePathListWithObjects(sourceResponse, targetResponse, CLASSIFICATIONS, pathNamesClassificationsPage, ID);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("rangeProviderClassificationFamilyIds")
+    void getClassificationFamilyNewNorwegian(int classificationFamilyId) {
+        String path = getClassificationFamilyByIdPath(classificationFamilyId);
+        Response sourceResponse = klassApiMigrationClient.getFromSourceApi(path, paramsLanguageNn, null);
+        Response targetResponse = klassApiMigrationClient.getFromTargetApi(path , paramsLanguageNn,null);
+
+        assertApiResponseIsNotNull(sourceResponse);
+
+        assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+
+        if(sourceResponse.getStatusCode() != 200) {
+            assertThat(compareError(null, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            validateItems(sourceResponse, targetResponse, pathNamesClassificationFamilyById);
+            validatePathListWithObjects(sourceResponse, targetResponse, CLASSIFICATIONS, pathNamesClassificationsPage, ID);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("rangeProviderClassificationFamilyIds")
+    void getClassificationFamilyNewNorwegianXml(int classificationFamilyId) {
+        String path = getClassificationFamilyByIdPath(classificationFamilyId);
+        Response sourceResponse = klassApiMigrationClient.getFromSourceApi(path, paramsLanguageNn,TEXT_XML);
+        Response targetResponse = klassApiMigrationClient.getFromTargetApi(path , paramsLanguageNn,TEXT_XML);
+
+        assertApiResponseIsNotNull(sourceResponse);
+
+        assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+
+        if(sourceResponse.getStatusCode() != 200) {
+            assertThat(compareError(null, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            validateItems(sourceResponse, targetResponse, pathNamesClassificationFamilyById);
+            validatePathListWithObjects(sourceResponse, targetResponse, CLASSIFICATIONS, pathNamesClassificationsPage, ID);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("rangeProviderClassificationFamilyIds")
+    void getClassificationFamilySsbSection(int classificationFamilyId) {
+
+        paramsSsbSection.put(SSB_SECTION, ssbSectionNames.get(randomSsbSectionId));
+        String path = getClassificationFamilyByIdPath(classificationFamilyId);
+        Response sourceResponse = klassApiMigrationClient.getFromSourceApi(path, paramsSsbSection, null);
+        Response targetResponse = klassApiMigrationClient.getFromTargetApi(path , paramsSsbSection,null);
+
+        assertApiResponseIsNotNull(sourceResponse);
+
+        assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+
+        if(sourceResponse.getStatusCode() != 200) {
+            assertThat(compareError(null, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            validateItems(sourceResponse, targetResponse, pathNamesClassificationFamilyById);
+            validatePathListWithObjects(sourceResponse, targetResponse, CLASSIFICATIONS, pathNamesClassificationsPage, ID);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("rangeProviderClassificationFamilyIds")
+    void getClassificationFamilySsbSectionXml(int classificationFamilyId) {
+        paramsSsbSection.put(SSB_SECTION, ssbSectionNames.get(randomSsbSectionId));
+
+        String path = getClassificationFamilyByIdPath(classificationFamilyId);
+        Response sourceResponse = klassApiMigrationClient.getFromSourceApi(path, paramsSsbSection,TEXT_XML);
+        Response targetResponse = klassApiMigrationClient.getFromTargetApi(path , paramsSsbSection,TEXT_XML);
 
         assertApiResponseIsNotNull(sourceResponse);
 
