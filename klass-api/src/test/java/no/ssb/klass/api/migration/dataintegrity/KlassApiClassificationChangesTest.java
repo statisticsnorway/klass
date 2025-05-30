1d4909db2d08b3a4875e49935e08cb8d7a2d4f6f
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-05-30 14:55:34 +0200

    add tests for all changes

--- klass-api/src/test/java/no/ssb/klass/api/migration/dataintegrity/KlassApiClassificationChangesTest.java
+++ klass-api/src/test/java/no/ssb/klass/api/migration/dataintegrity/KlassApiClassificationChangesTest.java
@@ -19,22 +19,33 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class KlassApiClassificationChangesTest extends AbstractKlassApiDataIntegrityTest {
 
+    static LocalDate date;
 
-    static String dateFromToMax;
+    static String dateFrom;
+    static String dateTo;
+
+    static Map<String, Object> paramsLanguageEn = new HashMap<>();
+    static Map<String, Object> paramsLanguageNn = new HashMap<>();
 
     static Map<String, Object> paramsDate = new HashMap<>();
+    static Map<String, Object> paramsDateInRange = new HashMap<>();
+    static Map<String, Object> paramsCsvSeparator = new HashMap<>();
 
     Response sourceResponse;
     Response targetResponse;
 
     @BeforeAll
     static void beforeAllCodes() {
-        dateFromToMax = generateRandomDate(
+        date = generateRandomDate(
                 LocalDate.of(1951, 1, 1),
-                LocalDate.of(2024, 12, 31)).format(formatter);
-
-
-        paramsDate.put(FROM, dateFromToMax);
+                LocalDate.of(2024, 12, 31));
+        dateFrom = date.format(formatter);
+        dateTo = generateRandomDate(date, LocalDate.of(2024, 12, 31)).format(formatter);
+        paramsDateInRange.putAll(Map.of(FROM, dateFrom, TO, dateTo));
+        paramsDate.put(FROM, dateFrom);
+        paramsLanguageEn.put(LANGUAGE, EN);
+        paramsLanguageNn.put(LANGUAGE, NN);
+        paramsCsvSeparator.putAll(Map.of(FROM, dateFrom, CSV_SEPARATOR, ";"));
 
     }
 
@@ -63,7 +74,7 @@ public class KlassApiClassificationChangesTest extends AbstractKlassApiDataInteg
 
     @ParameterizedTest
     @MethodSource("rangeProviderClassificationIds")
-    void getManyClassificationChanges(Integer classificationId) {
+    void getClassificationChanges(Integer classificationId) {
 
         System.out.println("Start test for ID " + classificationId + " at " + Instant.now());
 
@@ -85,8 +96,132 @@ public class KlassApiClassificationChangesTest extends AbstractKlassApiDataInteg
         System.out.println("End test for ID " + classificationId + " at " + Instant.now());
     }
 
+    @ParameterizedTest
+    @MethodSource("rangeProviderClassificationIds")
+    void getClassificationChangesXml(Integer classificationId) {
+
+        String path= getChangesPath(classificationId);
+        sourceResponse = klassApiMigrationClient.getFromSourceApi(path, paramsDate,TEXT_XML);
+        targetResponse = klassApiMigrationClient.getFromTargetApi(path, paramsDate,TEXT_XML);
+
+        assertApiResponseIsNotNull(sourceResponse);
+
+        assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+
+        if(sourceResponse.getStatusCode() != 200) {
+            assertThat(compareError(classificationId, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            validateList(sourceResponse, targetResponse, CODE_CHANGES);
+        }
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("rangeProviderClassificationIds")
+    void getClassificationChangesCsv(Integer classificationId) {
+
+        String path= getChangesPath(classificationId);
+        sourceResponse = klassApiMigrationClient.getFromSourceApi(path, paramsDate,TEXT_CSV);
+        targetResponse = klassApiMigrationClient.getFromTargetApi(path, paramsDate,TEXT_CSV);
+
+        assertApiResponseIsNotNull(sourceResponse);
+
+        assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+
+        if(sourceResponse.getStatusCode() != 200) {
+            assertThat(compareError(classificationId, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            validateList(sourceResponse, targetResponse, CODE_CHANGES);
+        }
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("rangeProviderClassificationIds")
+    void getClassificationChangesCsvParams(Integer classificationId) {
+
+        String path= getChangesPath(classificationId);
+        sourceResponse = klassApiMigrationClient.getFromSourceApi(path, paramsCsvSeparator,TEXT_CSV);
+        targetResponse = klassApiMigrationClient.getFromTargetApi(path, paramsCsvSeparator,TEXT_CSV);
+
+        assertApiResponseIsNotNull(sourceResponse);
+
+        assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+
+        if(sourceResponse.getStatusCode() != 200) {
+            assertThat(compareError(classificationId, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            validateList(sourceResponse, targetResponse, CODE_CHANGES);
+        }
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("rangeProviderClassificationIds")
+    void getClassificationChangesDatesInRange(Integer classificationId) {
+
+        String path= getChangesPath(classificationId);
+        sourceResponse = klassApiMigrationClient.getFromSourceApi(path, paramsDateInRange,null);
+        targetResponse = klassApiMigrationClient.getFromTargetApi(path, paramsDateInRange,null);
+
+        assertApiResponseIsNotNull(sourceResponse);
+
+        assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+
+        if(sourceResponse.getStatusCode() != 200) {
+            assertThat(compareError(classificationId, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            validateList(sourceResponse, targetResponse, CODE_CHANGES);
+        }
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("rangeProviderClassificationIds")
+    void getClassificationChangesEnglish(Integer classificationId) {
+
+        String path= getChangesPath(classificationId);
+        sourceResponse = klassApiMigrationClient.getFromSourceApi(path, paramsLanguageEn,null);
+        targetResponse = klassApiMigrationClient.getFromTargetApi(path, paramsLanguageEn,null);
+
+        assertApiResponseIsNotNull(sourceResponse);
+
+        assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+
+        if(sourceResponse.getStatusCode() != 200) {
+            assertThat(compareError(classificationId, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            validateList(sourceResponse, targetResponse, CODE_CHANGES);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("rangeProviderClassificationIds")
+    void getClassificationChangesNewNorwegian(Integer classificationId) {
+
+        String path= getChangesPath(classificationId);
+        sourceResponse = klassApiMigrationClient.getFromSourceApi(path, paramsLanguageNn,null);
+        targetResponse = klassApiMigrationClient.getFromTargetApi(path, paramsLanguageNn,null);
+
+        assertApiResponseIsNotNull(sourceResponse);
+
+        assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+
+        if(sourceResponse.getStatusCode() != 200) {
+            assertThat(compareError(classificationId, sourceResponse, targetResponse)).isTrue();
+        }
+        else{
+            validateList(sourceResponse, targetResponse, CODE_CHANGES);
+        }
+    }
+
     static Stream<Integer> rangeProviderClassificationIds() {
-        return IntStream.rangeClosed(0, 652).boxed();
+        return IntStream.rangeClosed(0, lastClassificationId).boxed();
     }
 
     String getChangesPath(Integer id) {
