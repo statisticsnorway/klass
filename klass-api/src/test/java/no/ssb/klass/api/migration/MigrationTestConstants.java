1434c7b5e310dbfe659248310ff31c7ad7a0b0b0
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-05-30 14:09:18 +0200

    get ssb section names

--- klass-api/src/test/java/no/ssb/klass/api/migration/MigrationTestConstants.java
+++ klass-api/src/test/java/no/ssb/klass/api/migration/MigrationTestConstants.java
@@ -31,6 +31,7 @@ public final class MigrationTestConstants {
     public static final String EMBEDDED_CLASSIFICATIONS = "_embedded.classifications";
     public static final String EMBEDDED_CLASSIFICATIONS_ID = "_embedded.classifications.id";
     public static final String EMBEDDED_SSB_SECTIONS= "_embedded.ssbSections";
+    public static final String EMBEDDED_SSB_SECTIONS_NAME= "_embedded.ssbSections.name";
     public static final String EMBEDDED_CLASSIFICATION_FAMILIES = "_embedded.classificationfamilies";
     public static final String PUBLISHED_NB="published.nb";
     public static final String PUBLISHED_EN="published.en";
