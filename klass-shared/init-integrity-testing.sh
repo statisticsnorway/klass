003e1322f7ce93dbaabd4a0f99606cd53ad61452
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-06-02 12:21:56 +0200

    add profile

--- klass-shared/init-integrity-testing.sh
+++ klass-shared/init-integrity-testing.sh
@@ -12,26 +12,26 @@ echo "Wait for mariadb to be ready"
 sleep 20
 
 echo "Load data from file to mariadb database"
-docker compose exec -T mariadb mariadb -u root -p"$MARIADB_ROOT_PASSWORD" --host=127.0.0.1 klass < "$MARIADB_LOCAL_FILEPATH"
+docker compose --profile migration-testing exec -T mariadb mariadb -u root -p"$MARIADB_ROOT_PASSWORD" --host=127.0.0.1 klass < "$MARIADB_LOCAL_FILEPATH"
 
 echo "Wait for load from file to finish"
 sleep 10
 
 echo "Load data from file to postgresql database"
-docker compose exec -T postgresql psql -U "$POSTGRES_USER" -d "klass" < "$POSTGRES_LOCAL_FILEPATH"
+docker compose --profile migration-testing exec -T postgresql psql -U "$POSTGRES_USER" -d "klass" < "$POSTGRES_LOCAL_FILEPATH"
 
 echo "Wait for load from file to finish"
 sleep 10
 
 echo "Build klass-api image"
-docker compose build
+docker compose --profile migration-testing build
 
 sleep 10
 
 echo "Restart klass api mariadb"
-docker compose restart klass-api-mariadb
+docker compose --profile migration-testing restart klass-api-mariadb
 
 sleep 10
 
 echo "Restart klass api"
-docker compose restart klass-api
+docker compose --profile migration-testing restart klass-api
