580297de7eab9c6a3da8fe673437147adfd7ac43
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-06-03 14:28:06 +0200

    add profile to script

--- klass-shared/init-migration.sh
+++ klass-shared/init-migration.sh
@@ -6,13 +6,13 @@ source .env
 set +a
 
 # Start containers
-docker compose up -d
+docker compose --profile migrate-data up -d
 
 echo "Wait for mariadb to be ready"
 sleep 10
 
 echo "Load data from file to mariadb database"
-docker compose exec -T mariadb mariadb -u root -p"$MARIADB_ROOT_PASSWORD" --host=127.0.0.1 klass < "$MARIADB_LOCAL_FILEPATH"
+docker compose --profile migrate-data exec -T mariadb mariadb -u root -p"$MARIADB_ROOT_PASSWORD" --host=127.0.0.1 klass < "$MARIADB_LOCAL_FILEPATH"
 
 echo "Wait for load from file to finish"
 sleep 10
@@ -26,7 +26,7 @@ sleep 10
 # Run necessary postgresql alteration on type timestamp to remove microseconds
 echo "Alter columns of type timestamp"
 docker cp alter_timestamp.sql "$(docker compose ps -q postgresql)":/tmp/alter_timestamp.sql
-docker compose exec -T postgresql psql -U klass -d klass -f /tmp/alter_timestamp.sql
+docker compose --profile migrate-data exec -T postgresql psql -U klass -d klass -f /tmp/alter_timestamp.sql
 
 echo "Dump data from postgres database to file"
 pg_dump -U klass -h localhost -p 5432 klass > "$POSTGRES_LOCAL_FILEPATH"/"$POSTGRES_DUMP_FILENAME"
