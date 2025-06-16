cbc5faee15911b33a9d402c7c2ccec37eb6fe169
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-06-16 12:45:02 +0200

    remove duplicated lines

--- klass-shared/src/main/java/no/ssb/klass/core/config/ConfigurationProfiles.java
+++ klass-shared/src/main/java/no/ssb/klass/core/config/ConfigurationProfiles.java
@@ -13,8 +13,6 @@ public final class ConfigurationProfiles {
         // Utility class
     }
 
-    public static final String POSTGRES_EMBEDDED = "postgres-embedded";
-
     /**
      * Profile for beans that should only be activated for the API application
      */
