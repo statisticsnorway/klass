9b33adca8716169b9c7d70e6e43fae4995103228
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-06-23 16:08:13 +0200

    Only redirect from /login

--- klass-forvaltning/src/main/java/no/ssb/klass/forvaltning/controllers/redirect/RedirectController.java
+++ klass-forvaltning/src/main/java/no/ssb/klass/forvaltning/controllers/redirect/RedirectController.java
@@ -1,19 +1,19 @@
 package no.ssb.klass.forvaltning.controllers.redirect;
 
+import no.ssb.klass.designer.ui.KlassUI;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
-@RequestMapping("/")
 public class RedirectController {
 
-    @GetMapping({"/", "/login"})
-    public RedirectView redirectWithUsingRedirectView(RedirectAttributes attributes) {
+    @GetMapping("/login")
+    public RedirectView redirectLogin(RedirectAttributes attributes) {
         attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
         attributes.addAttribute("attribute", "redirectWithRedirectView");
-        return new RedirectView("/klassui");
+        return new RedirectView("/" + KlassUI.PATH);
     }
+
 }
