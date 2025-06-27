91c266484ce94ec72643f13d579f8b87a3aa5d83
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-06-27 15:58:47 +0200

    Disable Hibernate logging

--- klass-forvaltning/src/main/java/no/ssb/klass/forvaltning/config/production/KlassVaadinConfiguration.java
+++ klass-forvaltning/src/main/java/no/ssb/klass/forvaltning/config/production/KlassVaadinConfiguration.java
@@ -1,13 +1,11 @@
 package no.ssb.klass.forvaltning.config.production;
 
+import com.vaadin.ui.declarative.Design;
+import no.ssb.klass.core.config.ConfigurationProfiles;
+import no.ssb.klass.designer.vaadin.VaadinSpringComponentFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import com.vaadin.ui.declarative.Design;
-
-import no.ssb.klass.core.config.ConfigurationProfiles;
-import no.ssb.klass.designer.vaadin.VaadinSpringComponentFactory;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
