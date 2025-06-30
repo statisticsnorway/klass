4bf43a0585d601b1ca8ac0768ab6c47d1636247b
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-06-30 15:24:30 +0200

    Redirect respects context path

--- klass-forvaltning/src/main/java/no/ssb/klass/forvaltning/controllers/redirect/RedirectController.java
+++ klass-forvaltning/src/main/java/no/ssb/klass/forvaltning/controllers/redirect/RedirectController.java
@@ -1,6 +1,7 @@
 package no.ssb.klass.forvaltning.controllers.redirect;
 
 import no.ssb.klass.designer.ui.KlassUI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -9,11 +10,20 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 public class RedirectController {
 
+    private final String serverContextPath;
+
+    public RedirectController(
+            @Value("${server.context-path}")
+            String serverContextPath
+    ) {
+        this.serverContextPath = serverContextPath;
+    }
+
     @GetMapping("/login")
     public RedirectView redirectLogin(RedirectAttributes attributes) {
         attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
         attributes.addAttribute("attribute", "redirectWithRedirectView");
-        return new RedirectView("/" + KlassUI.PATH);
+        return new RedirectView(this.serverContextPath + "/" + KlassUI.PATH);
     }
 
 }
