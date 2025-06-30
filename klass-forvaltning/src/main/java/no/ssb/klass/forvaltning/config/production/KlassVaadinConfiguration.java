7130a30729342f7f9e4c88e0faf55b510b7be5a6
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-06-30 10:16:21 +0200

    Klass-forvaltning tidying (#248)
    * Bump Spring Boot to 1.5.22
    
    * Use Vaadin core extensions, not security extensions
    
    * Bump Postgres driver
    
    * Prevent warnings about classpath scanning
    
    * Disable Hibernate logging
    
    * Remove unused dependencies
    
    * Remove unused dependency
    
    * Remove unused controllers
    
    * Configure prometheus metrics and health check
    
    * Deploy from PR
    
    * Revert "Deploy from PR"
    
    This reverts commit 38ed5de7543b580b35e9525a2b4fae8686deb248.
    
    * Reduce initial delay now that startup is quicker
    
    * Remove commented properties

--- klass-forvaltning/src/main/java/no/ssb/klass/forvaltning/config/production/KlassVaadinConfiguration.java
+++ klass-forvaltning/src/main/java/no/ssb/klass/forvaltning/config/production/KlassVaadinConfiguration.java
@@ -1,13 +1,11 @@
 package no.ssb.klass.forvaltning.config.production;
 
+import com.vaadin.ui.declarative.Design;
+import no.ssb.klass.core.config.ConfigurationProfiles;
+import no.ssb.klass.designer.vaadin.VaadinSpringComponentFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import com.vaadin.ui.declarative.Design;
-
-import no.ssb.klass.core.config.ConfigurationProfiles;
-import no.ssb.klass.designer.vaadin.VaadinSpringComponentFactory;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
@@ -16,10 +14,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class KlassVaadinConfiguration {
 
     @Bean
-    public VaadinSpringComponentFactory componentFactory() {
+    public static VaadinSpringComponentFactory componentFactory() {
         VaadinSpringComponentFactory componentFactory = new VaadinSpringComponentFactory();
         Design.setComponentFactory(componentFactory);
         return componentFactory;
     }
-
 }
