5647bea32787e95c57c82a93ee461c1565924775
Jorgen-5 <rlj@ssb.no>
2025-07-01 12:34:24 +0200

    Change url

--- klass-solr/src/main/java/no/ssb/klass/solr/config/KlassSearchConfiguration.java
+++ klass-solr/src/main/java/no/ssb/klass/solr/config/KlassSearchConfiguration.java
@@ -35,7 +35,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 public class KlassSearchConfiguration implements AsyncConfigurer {
 
     @NotEmpty
-    @Value("${spring.data.solr.host}")
+    @Value("${klass.env.search.solr.url}")
     protected String solrUrl;
     @NotEmpty
     @Value("${klass.env.search.solr.core}")
@@ -44,7 +44,7 @@ public class KlassSearchConfiguration implements AsyncConfigurer {
     @Bean
     @Profile("remote-solr")
     public SolrClient solrClient() {
-        HttpSolrClient httpSolrClient = new SolrBackwardsCompatibleHttpClient(solrUrl + "/Klass");
+        HttpSolrClient httpSolrClient = new SolrBackwardsCompatibleHttpClient(solrUrl);
         httpSolrClient.setRequestWriter(new RequestWriter());
         return httpSolrClient;
     }
