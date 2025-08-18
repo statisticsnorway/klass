da116165ec00e2e432d74a16651688b47aa02c61
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-08-18 21:45:24 +0200

    add test

--- klass-api/src/test/java/no/ssb/klass/api/migration/section/KlassApiSectionMigrationTest.java
+++ klass-api/src/test/java/no/ssb/klass/api/migration/section/KlassApiSectionMigrationTest.java
@@ -4,12 +4,15 @@ import io.restassured.response.Response;
 import no.ssb.klass.api.migration.dataintegrity.AbstractKlassApiDataIntegrityTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static no.ssb.klass.api.migration.MigrationTestConstants.*;
 import static no.ssb.klass.api.migration.MigrationTestUtils.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class KlassApiSectionMigrationTest extends AbstractKlassApiDataIntegrityTest {
 
-    // Test that all mapping is ok
     @Test
     void getSsbSections(){
         String path = getSsbSectionsPath();
@@ -19,6 +22,17 @@ public class KlassApiSectionMigrationTest extends AbstractKlassApiDataIntegrityT
         assertApiResponseIsNotNull(sourceResponse);
 
         assertStatusCodesEqual(sourceResponse.getStatusCode(), targetResponse.getStatusCode(), path);
+        List<Map<String, Object>> sourceList = sourceResponse.path("_embedded.ssbSections");
+        List<Map<String, Object>> targetList = targetResponse.path("_embedded.ssbSections");
+
+        for (int i = 0; i < sourceList.size(); i++){
+            String sourceName = sourceList.get(i).get("name").toString();
+            String targetName = targetList.get(i).get("name").toString();
+            if(sourceList.get(i).get("name") != null){
+                assertThat(sourceName).startsWith(targetName);
+            }
+        }
+
 
     }
     String getSsbSectionsPath() {
