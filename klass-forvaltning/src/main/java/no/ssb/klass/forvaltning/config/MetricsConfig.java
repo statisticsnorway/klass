c0cc1dae591866e25cad33269e9c7a674f070f62
Jorgen-5 <rlj@ssb.no>
2025-08-26 15:14:50 +0200

    Add log

--- klass-forvaltning/src/main/java/no/ssb/klass/forvaltning/config/MetricsConfig.java
+++ klass-forvaltning/src/main/java/no/ssb/klass/forvaltning/config/MetricsConfig.java
@@ -2,16 +2,19 @@ package no.ssb.klass.forvaltning.config;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.spring.autoconfigure.MeterRegistryCustomizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MetricsConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(MetricsConfig.class);
+
     @Bean
     public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-        System.out.println(">>> MetricsConfig loaded!"); // debug
-
+        logger.info(">>> MetricsConfig loaded!");
         return registry -> registry.config().commonTags("application", "klass-forvaltning");
     }
 }
