89e17606fff64e63618ae99cec2d40a67eb0c7d1
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-10-29 12:50:44 +0100

    Fix application of CORS header (#401)

--- klass-api/src/main/java/no/ssb/klass/api/filters/AccessFilter.java
+++ klass-api/src/main/java/no/ssb/klass/api/filters/AccessFilter.java
@@ -2,36 +2,31 @@ package no.ssb.klass.api.filters;
 
 import no.ssb.klass.api.util.RestConstants;
 
+import java.io.IOException;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebFilter("/*")
 public class AccessFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest request,
-                         ServletResponse response,
-                         FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
 
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
 
         String path = req.getServletPath();
 
-        if (path.startsWith(RestConstants.API_VERSION_V1)) {
-
-            if (path.startsWith(RestConstants.CONTEXT_AND_VERSION_V1)) {
-                res.addHeader("Access-Control-Allow-Origin", "*");
-                res.addHeader("Vary", "Accept");
-                res.addHeader("Vary", "X-Forwarded-Proto");
-            }
-
+        // Allow Klass web and other frontends to serve data from the API
+        if (path.startsWith(RestConstants.CONTEXT_PATH)) {
+            res.addHeader("Access-Control-Allow-Origin", "*");
+            res.addHeader("Vary", "Accept");
+            res.addHeader("Vary", "X-Forwarded-Proto");
         }
-
         chain.doFilter(request, response);
     }
-
 }
