83d8792448b5a2941dab5f5f1e8bbbc1d49f9c6f
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-11-13 11:06:51 +0100

    Structured logging in cluster only (#420)
    * Structured logging in cluster only
    
    This makes it easier to view logs locally
    
    - Remove previous logging config
    - Use default setup running locally
    - Set LOGGING_STRUCTURED_FORMAT_CONSOLE to logstash in the cluster
    
    * Configure tomcat access logging
    
    * Exclude paths from access logs
    
    * Code format

---
+++ klass-api/src/main/java/no/ssb/klass/api/config/TomcatAccessLogConfig.java
@@ -0,0 +1,22 @@
+package no.ssb.klass.api.config;
+
+import ch.qos.logback.access.tomcat.LogbackValve;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatAccessLogConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> accessLogsCustomizer() {
+        return factory -> {
+            var logbackValve = new LogbackValve();
+            logbackValve.setFilename("logback-access.xml");
+            logbackValve.setAsyncSupported(true);
+            factory.addContextValves(logbackValve);
+        };
+    }
+}
