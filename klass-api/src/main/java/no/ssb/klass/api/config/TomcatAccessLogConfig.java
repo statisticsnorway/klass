06c07d878e117a7ba5a819c3d0cf4c8ef2ab705d
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-11-13 10:13:06 +0100

    Code format

--- klass-api/src/main/java/no/ssb/klass/api/config/TomcatAccessLogConfig.java
+++ klass-api/src/main/java/no/ssb/klass/api/config/TomcatAccessLogConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TomcatAccessLogConfig {
 
-
     @Bean
     public WebServerFactoryCustomizer<TomcatServletWebServerFactory> accessLogsCustomizer() {
         return factory -> {
