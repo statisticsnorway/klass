f71036134ad6be3202427f89351d37e78ea9aab8
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-11-25 09:56:29 +0100

    Try another method of avoiding jsessionid in URL (#452)

--- klass-forvaltning/src/main/java/no/ssb/klass/forvaltning/controllers/redirect/RedirectController.java
+++ klass-forvaltning/src/main/java/no/ssb/klass/forvaltning/controllers/redirect/RedirectController.java
@@ -5,26 +5,22 @@ import no.ssb.klass.designer.ui.KlassUI;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class RedirectController {
 
     private final String serverContextPath;
 
-    public RedirectController(
-            @Value("${server.context-path}")
-            String serverContextPath
-    ) {
+    public RedirectController(@Value("${server.context-path}") String serverContextPath) {
         this.serverContextPath = serverContextPath;
     }
 
     @GetMapping("/login")
-    public RedirectView redirectLogin(RedirectAttributes attributes) {
-        attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
-        attributes.addAttribute("attribute", "redirectWithRedirectView");
-        return new RedirectView(this.serverContextPath + "/" + KlassUI.PATH, false);
+    public void redirectLogin(HttpServletResponse httpServletResponse) throws IOException {
+        httpServletResponse.sendRedirect(this.serverContextPath + "/" + KlassUI.PATH);
     }
-
 }
