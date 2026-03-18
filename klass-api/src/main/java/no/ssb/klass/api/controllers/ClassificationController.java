22d7ed1344d5e762e75faf5372f703369cae4bb0
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-03-18 16:21:04 +0100

    format

--- klass-api/src/main/java/no/ssb/klass/api/controllers/ClassificationController.java
+++ klass-api/src/main/java/no/ssb/klass/api/controllers/ClassificationController.java
@@ -519,7 +519,7 @@ public class ClassificationController {
 
             ClassificationSeries classification = classificationService.getClassificationSeries(id);
             successGetClassification = classification != null;
-            startGetChangeTables= System.currentTimeMillis();
+            startGetChangeTables = System.currentTimeMillis();
             List<CorrespondenceTable> changeTables =
                     classification.getChangeTables(dateRange, includeFuture).stream()
                             .filter(
