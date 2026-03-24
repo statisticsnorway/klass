0e25a8b5ebd82dfe7103956247303e7bd0b3b669
Jorgen-5 <rlj@ssb.no>
2026-03-24 20:34:46 +0100

    Fix error

--- klass-forvaltning/src/main/java/no/ssb/klass/auth/JwtAuthFilter.java
+++ klass-forvaltning/src/main/java/no/ssb/klass/auth/JwtAuthFilter.java
@@ -27,6 +27,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
