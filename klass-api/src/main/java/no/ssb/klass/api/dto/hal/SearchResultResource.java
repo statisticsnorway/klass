ec932e9d93ddc17dff71dc234f77283969483206
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-06-12 19:02:56 +0200

    expose language for search result resource

--- klass-api/src/main/java/no/ssb/klass/api/dto/hal/SearchResultResource.java
+++ klass-api/src/main/java/no/ssb/klass/api/dto/hal/SearchResultResource.java
@@ -12,15 +12,17 @@ import org.springframework.hateoas.server.core.Relation;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.StringJoiner;
 
 @Relation(collectionRelation = "searchResults")
 @JsonPropertyOrder({"name", "id", "snippet", "searchScore", "links"})
 public class SearchResultResource extends KlassResource {
 
-    private String name;
+    private final String name;
     private String snippet;
-    private Double searchScore;
+    private final Double searchScore;
+    private final String language;
 
     public SearchResultResource(
             OpenSearchResult searchResult, Map<String, List<String>> highlights) {
@@ -28,12 +30,13 @@ public class SearchResultResource extends KlassResource {
         this.name = searchResult.getTitle();
         this.snippet = searchResult.getDescription();
         this.searchScore = searchResult.getScore();
+        this.language = searchResult.getLanguage();
         addLink(createSelfLink(searchResult.getItemId()));
 
         if (highlights != null) {
             List<String> descriptionHighlights = highlights.get("description");
             if (descriptionHighlights != null && !descriptionHighlights.isEmpty()) {
-                String snipplet = descriptionHighlights.get(0);
+                String snipplet = descriptionHighlights.getFirst();
                 if (snipplet.contains("<strong>")) {
                     this.snippet = snipplet;
                     return;
@@ -42,7 +45,7 @@ public class SearchResultResource extends KlassResource {
 
             List<String> codesHighlights = highlights.get("codes");
             if (codesHighlights != null && !codesHighlights.isEmpty()) {
-                String snipplet = codesHighlights.get(0);
+                String snipplet = codesHighlights.getFirst();
                 if (snipplet.contains("<strong>")) {
                     StringJoiner joiner = new StringJoiner(", ");
                     codesHighlights.forEach(joiner::add);
@@ -58,12 +61,29 @@ public class SearchResultResource extends KlassResource {
                 .expand();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        SearchResultResource that = (SearchResultResource) o;
+        return Objects.equals(name, that.name) && Objects.equals(snippet, that.snippet) && Objects.equals(searchScore, that.searchScore) && Objects.equals(language, that.language);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name, snippet, searchScore, language);
+    }
+
     public String getName() {
         return name;
     }
 
     public String getSnippet() {
         return snippet;
+    }
+
+    public String getLanguage() {
+        return language;
     }
 
     public Double getSearchScore() {
