a8a4b33bd681b6f03ff921f0c8f5274508586f12
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-06-15 11:33:23 +0200

    add test data

--- klass-api/src/test/java/no/ssb/klass/api/applicationtest/RestApiListClassificationIntegrationTest.java
+++ klass-api/src/test/java/no/ssb/klass/api/applicationtest/RestApiListClassificationIntegrationTest.java
@@ -51,7 +51,7 @@ class RestApiListClassificationIntegrationTest extends AbstractRestApiApplicatio
                 .body(JSON_LINKS + ".search.href", containsString(REQUEST_SEARCH))
                 // paging
                 .body(JSON_PAGE + ".size", equalTo(20))
-                .body(JSON_PAGE + ".totalElements", equalTo(4))
+                .body(JSON_PAGE + ".totalElements", equalTo(5))
                 .body(JSON_PAGE + ".totalPages", equalTo(1))
                 .body(JSON_PAGE + ".number", equalTo(0));
     }
@@ -132,7 +132,7 @@ class RestApiListClassificationIntegrationTest extends AbstractRestApiApplicatio
                 .body(JSON_LINKS + ".search.href", containsString(REQUEST_SEARCH))
                 // paging
                 .body(JSON_PAGE + ".size", equalTo(20))
-                .body(JSON_PAGE + ".totalElements", equalTo(6))
+                .body(JSON_PAGE + ".totalElements", equalTo(7))
                 .body(JSON_PAGE + ".totalPages", equalTo(1))
                 .body(JSON_PAGE + ".number", equalTo(0));
     }
@@ -204,7 +204,7 @@ class RestApiListClassificationIntegrationTest extends AbstractRestApiApplicatio
                 .body(XML_ROOT + ".link[1].href", containsString(REQUEST_SEARCH))
                 // paging
                 .body(XML_PAGE + ".size.toInteger();", equalTo(20))
-                .body(XML_PAGE + ".totalElements.toInteger();", equalTo(4))
+                .body(XML_PAGE + ".totalElements.toInteger();", equalTo(5))
                 .body(XML_PAGE + ".totalPages.toInteger();", equalTo(1))
                 .body(XML_PAGE + ".number.toInteger();", equalTo(0));
     }
@@ -244,7 +244,7 @@ class RestApiListClassificationIntegrationTest extends AbstractRestApiApplicatio
                 .body(XML_ROOT + ".link[1].href", containsString(REQUEST_SEARCH))
                 // paging
                 .body(XML_PAGE + ".size.toInteger();", equalTo(20))
-                .body(XML_PAGE + ".totalElements.toInteger();", equalTo(6))
+                .body(XML_PAGE + ".totalElements.toInteger();", equalTo(7))
                 .body(XML_PAGE + ".totalPages.toInteger();", equalTo(1))
                 .body(XML_PAGE + ".number.toInteger();", equalTo(0));
     }
