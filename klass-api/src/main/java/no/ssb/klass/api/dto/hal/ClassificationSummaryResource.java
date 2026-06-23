201f4e6bc4d98b0f3d212a67ff686ef7b82d70e7
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-06-23 10:25:22 +0200

    Add optional param include classifications description (#648)
    * add optional parameter include description
    
    * add test
    
    * lint
    
    * update api docs
    
    * import annotation
    
    * move annotation to getter
    
    * remove public modifier ref: sonar scan
    
    * linting

--- klass-api/src/main/java/no/ssb/klass/api/dto/hal/ClassificationSummaryResource.java
+++ klass-api/src/main/java/no/ssb/klass/api/dto/hal/ClassificationSummaryResource.java
@@ -1,10 +1,13 @@
 package no.ssb.klass.api.dto.hal;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import static java.util.stream.Collectors.*;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import jakarta.validation.constraints.NotNull;
@@ -18,6 +21,7 @@ import org.springframework.hateoas.server.core.Relation;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Relation(collectionRelation = "classifications")
 @JsonPropertyOrder({
@@ -26,6 +30,7 @@ import java.util.List;
     "classificationType",
     "classificationFamilyId",
     "lastModified",
+    "description",
     "links"
 })
 public class ClassificationSummaryResource extends KlassResource {
@@ -33,8 +38,14 @@ public class ClassificationSummaryResource extends KlassResource {
     private final String classificationType;
     private final Long classificationFamilyId;
     private final Date lastModified;
+    private final String description;
 
     public ClassificationSummaryResource(Language language, ClassificationSeries classification) {
+        this(language, classification, false);
+    }
+
+    public ClassificationSummaryResource(
+            Language language, ClassificationSeries classification, boolean includeDescription) {
         super(classification.getId());
         @NotNull
         Language chosenLanguage = language != null ? language : classification.getPrimaryLanguage();
@@ -43,6 +54,8 @@ public class ClassificationSummaryResource extends KlassResource {
                 classification.getClassificationType().getDisplayName(chosenLanguage);
         this.classificationFamilyId = classification.getClassificationFamilyId();
         this.lastModified = classification.getLastModified();
+        this.description =
+                includeDescription ? classification.getDescription(chosenLanguage) : null;
         addLink(createSelfLink(classification.getId()));
     }
 
@@ -50,6 +63,11 @@ public class ClassificationSummaryResource extends KlassResource {
         return linkTo(methodOn(ClassificationController.class).classification(id, null, null))
                 .withSelfRel()
                 .expand();
+    }
+
+    @JsonInclude(NON_NULL)
+    public String getDescription() {
+        return description;
     }
 
     public String getClassificationType() {
@@ -74,5 +92,28 @@ public class ClassificationSummaryResource extends KlassResource {
         return classifications.stream()
                 .map(classification -> new ClassificationSummaryResource(language, classification))
                 .collect(toList());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ClassificationSummaryResource that = (ClassificationSummaryResource) o;
+        return Objects.equals(name, that.name)
+                && Objects.equals(classificationType, that.classificationType)
+                && Objects.equals(classificationFamilyId, that.classificationFamilyId)
+                && Objects.equals(lastModified, that.lastModified)
+                && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                super.hashCode(),
+                name,
+                classificationType,
+                classificationFamilyId,
+                lastModified,
+                description);
     }
 }
