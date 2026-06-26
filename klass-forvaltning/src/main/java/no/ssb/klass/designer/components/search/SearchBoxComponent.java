0076c7e1c45d1b3640e649175b7a7f9bc0d803f9
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-06-26 19:01:13 +0200

    delete search view and related components

--- klass-forvaltning/src/main/java/no/ssb/klass/designer/components/search/SearchBoxComponent.java
+++ klass-forvaltning/src/main/java/no/ssb/klass/designer/components/search/SearchBoxComponent.java
@@ -1,14 +1,9 @@
 package no.ssb.klass.designer.components.search;
 
-import com.google.common.collect.ImmutableMap;
-import com.vaadin.data.Validator;
 import com.vaadin.data.validator.StringLengthValidator;
 import com.vaadin.ui.AbstractTextField;
 import com.vaadin.ui.CustomComponent;
 
-import no.ssb.klass.designer.ClassificationSearchView;
-import no.ssb.klass.designer.listeners.EnterListener;
-import no.ssb.klass.designer.util.VaadinUtil;
 
 /**
  * @author Mads Lundemo, SSB.
@@ -19,20 +14,7 @@ public class SearchBoxComponent extends CustomComponent {
 
     public SearchBoxComponent() {
         setCompositionRoot(design);
-        configureListeners();
         configureSearchBoxComponents();
-    }
-
-    private void configureListeners() {
-        design.searchButton.addClickListener(event -> gotoSearchView());
-        design.searchField.addTextChangeListener(event -> design.searchField.setValidationVisible(false));
-        design.searchField.addShortcutListener(new EnterListener(design.searchField) {
-            @Override
-            protected void enterPressed() {
-                gotoSearchView();
-            }
-        });
-
     }
 
     public void setSearchText(String text) {
@@ -49,14 +31,5 @@ public class SearchBoxComponent extends CustomComponent {
 
     }
 
-    private void gotoSearchView() {
-        try {
-            design.searchField.validate();
-            VaadinUtil.navigateTo(ClassificationSearchView.NAME, ImmutableMap.of(ClassificationSearchView.PARAM_QUERY,
-                    design.searchField.getValue(), "r", String.valueOf(Math.random())));
-        } catch (Validator.InvalidValueException e) {
-            design.searchField.setValidationVisible(true);
-        }
-    }
 
 }
